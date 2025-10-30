7e2bb5cd1f979970dbeb0e5d3b6cc27ff77af7b9
Daniel Antonio <tu_correo@example.com>
2025-10-30 15:20:34 -0300

    REORDEN DIRECTORIOS

--- modulo4/tabla_multiplicar.py
+++
@@ -1,5 +0,0 @@
-n = int(input("¿Qué tabla quieres ver? "))  # Entero
-
-print(f"Tabla del {n}:")
-for i in range(1, 11):                      # Bucle for del 1 al 10
-    print(f"{n} x {i} = {n * i}")
